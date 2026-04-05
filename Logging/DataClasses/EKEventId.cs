28768704d6651f3ffede5da46cd0d78142ec0fd9
j.katzwinkel@posteo.de <j.katzwinkel@posteo.de>
2026-04-05 02:12:43 +0200

    Removed unused EKEventId class

--- Logging/DataClasses/EKEventId.cs
+++
@@ -1,8 +0,0 @@
-using Echotools.Logging.Enums;
-
-namespace Echotools.Logging.DataClasses;
-
-public class EKEventId : EchoEventId
-{
-    public EKEventId(int id, TextSource textSource) : base(id, textSource) { }
-}
